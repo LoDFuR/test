95d0a7526ded22083a6caf4c97a77baca2763284
LoDFuR <LoDFuR1@gmail.com>
2023-10-11 00:58:43 +0300

    One

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
        
         public static void main(String[] args){
  		// write your code here
+		System.out.println("1 One");
 		System.out.println("HELLO WORLD!");
 		     }
 }
